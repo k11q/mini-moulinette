58ab579062fbd7a0d079e182641c4b105f802cc0
Khairul Haaziq <rhycoz@yahoo.com>
2023-02-16 13:24:11 +0800

    add

--- 41test/test.sh
+++ 41test/test.sh
@@ -9,8 +9,12 @@ echo "|                        41 TESTS|"
 echo "+================================+"
 echo -e "\033[0m"
 
-GREEN='\033[32m'
-RED='\033[31m'
+GREEN='\033[92m'
+RED='\033[101m'
+BLUE='\033[94m'
+BG_GREEN='\033[42m'
+BG_RED='\033[38;5;8m[1]'
+GREY='\033[38;5;8m'
 DEFAULT='\033[0m'
 CHECKS=0
 PASSED=0
@@ -25,65 +29,32 @@ fi
 echo "Collected files:"
 ls ../* | grep -v "../41test:*" | grep -v "../41test" | column
 #find .. -not -path "../41test/*" -not -path "../.*" -type f | sed 's/^..\///' | column | sort -t'/' -k1,1 -k2,2
-echo -e ""
 if [ "${1}" = "C02" ]; then
 CHECKS=$((CHECKS+1))
+echo -e ""
+echo -e "${BLUE}ex00/ft_strcpy.c${DEFAULT}"
+echo -e ""
 if [ -f "${SCRIPT_DIR}/../ex00/ft_strcpy.c" ]; then
-    echo -e "${GREEN}ft_strcpy.c exists.${DEFAULT}"
+    echo -e "${BG_GREEN} PASS ${DEFAULT}""${GREY} [1] ./ft_strcpy.c exists.${DEFAULT}"
     if cc ${SCRIPT_DIR}/C02/ex00.c 2> /dev/null; then
     if ./a.out = 0; then
-    	echo -e "\033[42m PASSED \033[0m""\033[38;5;8m ex00/\033[0mft_strcpy.c"
-      echo -e "${GREEN}  \xE2\x9C\x93 \033[38;5;8m[1] ./ft_strcpy can be compiled. ${DEFAULT}"
-	    echo -e "${GREEN}  \xE2\x9C\x93 \033[38;5;8m[2] Output should be squariyoh ${DEFAULT}"
+      echo -e "${BG_GREEN} PASS ${DEFAULT}""${GREY} [2] ./ft_strcpy can be compiled.${DEFAULT}"
+	    echo -e "${BG_GREEN} PASS ${DEFAULT}""${GREY} [3] Output should be squariyoh.${DEFAULT}"
+      #echo -e "\033[42m PASSED \033[0m""\033[38;5;8m ex00/\033[0mft_strcpy.c"
+      PASSED=$((PASSED+1))
     else
         echo "failed"
     fi
     rm a.out
     else
-    	echo "cannot compile"
+    	echo -e " \033[38;5;8m[1] ./ft_strcpy.c does not compile.  ${RED}FAILED${DEFAULT}"
     fi
 else
-    echo -e "${RED}ft_strcpy.c does not exist.${DEFAULT}"
+    echo -e " \033[38;5;8m[1] ./ft_strcpy.c does not exists.  ${RED}FAILED${DEFAULT}"
 fi
 echo -e ""
-CHECKS=$((CHECKS+1))
-if [ -f "${SCRIPT_DIR}/../ex01/ft_strncpy.c" ]; then
-    echo -e "${GREEN}ft_strncpy.c exists.${DEFAULT}"
-    if cc ${SCRIPT_DIR}/C02/ex01.c 2> /dev/null; then
-    if ./a.out = 0; then
-    	echo -e "\033[42m PASSED \033[0m""\033[38;5;8m ex00/\033[0mft_strncpy.c"
-      echo -e "${GREEN}  \xE2\x9C\x93 \033[38;5;8m[1] ./ft_strncpy can be compiled. ${DEFAULT}"
-	    echo -e "${GREEN}  \xE2\x9C\x93 \033[38;5;8m[2] Output should be squariyoh ${DEFAULT}"
-    else
-        echo "failed"
-    fi
-    rm a.out
-    else
-    	echo "cannot compile"
-    fi
-else
-    echo -e "${RED}ft_strncpy.c does not exist.${DEFAULT}"
-fi
+echo -e "Result:  ${GREEN}OK :D ${GREY}ex00/${DEFAULT}ft_strcpy.c"
 echo -e ""
-CHECKS=$((CHECKS+1))
-if [ -f "${SCRIPT_DIR}/../ex02/ft_str_is_alpha.c" ]; then
-    echo -e "${GREEN}ft_str_is_alpha.c exists.${DEFAULT}"
-    if cc ${SCRIPT_DIR}/C02/ex02.c 2> /dev/null; then
-    if ./a.out = 0; then
-    	echo -e "\033[42m PASSED \033[0m""\033[38;5;8m ex00/\033[0mft_str_is_alpha.c"
-	echo -e "${GREEN}  \xE2\x9C\x93 \033[38;5;8m[1] Output should be squariyoh ${DEFAULT}"
-    else
-        echo "failed"
-    fi
-    rm a.out
-    else
-    	echo "cannot compile"
-    fi
-else
-    echo -e "${RED}ft_str_is_alpha.c does not exist.${DEFAULT}"
-fi
-echo -e ""
-PASSED=$((PASSED+1))
 PERCENT=$((100 * PASSED / CHECKS))
 
 echo -e "\033[38;5;8mTotal checks:  ${DEFAULT}""${GREEN}${PASSED} passed  ${DEFAULT} ""${CHECKS} total"
