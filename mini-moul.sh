36ab275d14419b613c098517dddc61189c0858a2
Khairul Haaziq <rhycoz@yahoo.com>
2023-02-18 10:57:19 +0800

    add

--- mini-moul.sh
+++ mini-moul.sh
@@ -1,9 +1,13 @@
-source mini-moul/config.sh
-
-cd mini-moul
+source ~/mini-moulinette/mini-moul/config.sh
 
 if [[ "$#" -eq 1 ]]; then
+	cp -R ~/mini-moulinette/mini-moul mini-moul
+	cd mini-moul
   ./test.sh "$1"
+  rm -R ../mini-moul
 else
   printf "${RED}You need to choose an assignment. e.g: ./mini-moul.sh C02\n${DEFAULT}"
+
 fi
+
+exit 1
