4b835fdde87afedd88f5a81006df4a4d13be891d
Khairul Haaziq <rhycoz@yahoo.com>
2023-02-18 12:42:51 +0800

    add

--- mini-moul/tests/C02/ex01/ft_strncpy.c
+++ mini-moul/tests/C02/ex01/ft_strncpy.c
@@ -32,11 +32,11 @@ int	test2(void)
 
 	if (strcmp(result, "Hello1") != 0)
 	{
-		printf("    " RED "[2] ft_strncpy(\"World1\", \"Hello\", 3) Expected \"Hello1\", got \"%s\"\n", result);
+		printf("    " RED "[2] ft_strncpy(\"World1\", \"Hello\", 5) Expected \"Hello1\", got \"%s\"\n", result);
 		return (-1);
 	}
 	else
-		printf("  " GREEN CHECKMARK GREY " [2] ft_strncpy(\"World1\", \"Hello\", 3) Expected \"Hello1\", got \"%s\"\n" DEFAULT, result);
+		printf("  " GREEN CHECKMARK GREY " [2] ft_strncpy(\"World1\", \"Hello\", 5) Expected \"Hello1\", got \"%s\"\n" DEFAULT, result);
 	return (0);
 }
 
@@ -50,11 +50,11 @@ int	test3(void)
 
 	if (strcmp(result, "Hello") != 0)
 	{
-		printf("    " RED "[3] ft_strncpy(\"World1\", \"Hello\", 3) Expected \"Hello\", got \"%s\"\n", result);
+		printf("    " RED "[3] ft_strncpy(\"World1\", \"Hello\", 7) Expected \"Hello\", got \"%s\"\n", result);
 		return (-1);
 	}
 	else
-		printf("  " GREEN CHECKMARK GREY " [3] ft_strncpy(\"World1\", \"Hello\", 3) Expected \"Hello\", got \"%s\"\n" DEFAULT, result);
+		printf("  " GREEN CHECKMARK GREY " [3] ft_strncpy(\"World1\", \"Hello\", 7) Expected \"Hello\", got \"%s\"\n" DEFAULT, result);
 	return (0);
 }
 
