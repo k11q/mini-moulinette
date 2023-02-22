e41cd085ed9dbb13d377ccaf1f87a22288e31ad1
Khairul Haaziq <rhycoz@yahoo.com>
2023-02-22 17:08:34 +0800

    add

--- mini-moul/tests/C08/ex05/ft_show_tab.c
+++ mini-moul/tests/C08/ex05/ft_show_tab.c
@@ -1,29 +1,14 @@
 #include <stdlib.h>
 #include <stdio.h>
 #include <string.h>
-#include "ft_stock_str.h"
+#include "resources/ft_strs_to_tab.c"
+#include "resources/ft_stock_str.h"
 #include "../../../../ex05/ft_show_tab.c"
 #include "../../../utils/constants.h"
 
-int main(void)
+
+int	main(int argc, char **argv)
 {
-    char *strings[] = {
-        "Hello",
-        "World",
-        "",
-        NULL
-    };
-    int size = 4;
-    t_stock_str *stock_str = ft_strs_to_tab(size, strings);
-    ft_show_tab(stock_str);
-
+	ft_show_tab(ft_strs_to_tab(argc, argv));
     printf("  " GREEN CHECKMARK GREY " File was able to compile.\n" DEFAULT);
-
-    for (int i = 0; i < size; i++)
-    {
-        free(stock_str[i].copy);
-    }
-    free(stock_str);
-
-    return 0;
 }
